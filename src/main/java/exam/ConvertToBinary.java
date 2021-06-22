5b4be7b33b570a55d169fc9d1797d94114e9c2fb
vaishnavi4517 <47924733+vaishnavi4517@users.noreply.github.com>
2021-06-22 12:36:53 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,35 @@
 package exam;
-
+import java.io.*;
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+  
+    
+    public String toBinary(final int n) {
+    
+        // Creating and assigning binary array size
+        int[] binary = new int[35];
+        int id = 0;
+  
+        // Number should be positive
+        while (n > 0) {
+            binary[id++] = n % 2;
+            n = n / 2;
+        }
+         return "Array.toString(binary)";
+        
+    }
+  
+
+    public static void main(String[] args)
+    {
+        
+        int num = 45;
+  
+        
+        String a=toBinary(num);
+      System.out.println("a");
+    }
 }
+   
+  
+
